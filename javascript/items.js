c341156b97e85c51184bea2cc97c3730a9f5ffe3
rebeccahsu <rebeccahsuhth@gmail.com>
2022-02-01 23:05:34 +0800

    items overlay js

--- javascript/items.js
+++ javascript/items.js
@@ -1,33 +1,50 @@
 // -------- Overlay ----------
 
 var item = document.getElementsByClassName("item");
-console.log(item);
+// console.log(item);
 // var item2 = document.querySelectorAll(".item");
 // console.log(item2);
 let overlay = document.getElementById("overlay");
 // console.log(overlay);
 let mask = document.getElementById("mask");
 
-/*
-document.querySelectorAll(".item").forEach(function(item){
-    item.addEventListener('click', function(){
-        mask.classList.remove("noshow");
-        mask.classList.add("show");
-        overlay.classList.remove("noshow");
-        overlay.classList.add("show");
-    })
-})
-*/
+var opic = document.getElementById("opic");
+
+var itemname = document.getElementsByClassName("itemname");
+var oname = document.getElementById("oname");
+
+var ocolor = document.getElementById("ocolor");
+var osize = document.getElementById("osize");
+var omaterial = document.getElementById("omaterial");
+
 
 for(let i = 0; i < item.length; i++){
     // console.log(item[i]);
 
     item[i].addEventListener("click", function(){
         // console.log("clicked");
+        opic.innerHTML = "";
+
         mask.classList.remove("noshow");
         mask.classList.add("show");
         overlay.classList.remove("noshow");
         overlay.classList.add("show");
+
+        var oitemname = itemname[i].innerHTML;
+        oname.innerHTML = oitemname;
+
+        var oitemcolor = document.getElementsByClassName("icolor")[i].innerHTML;
+        ocolor.innerHTML = oitemcolor;
+
+        var oitemsize = document.getElementsByClassName("size")[i].innerHTML;
+        osize.innerHTML = oitemsize;
+
+        var oitemmaterial = document.getElementsByClassName("material")[i].innerHTML;
+        omaterial.innerHTML = oitemmaterial;
+
+        var oitempic = document.getElementsByClassName("ipic")[i].src;
+        opic.insertAdjacentHTML("afterbegin", '<img src="'+ oitempic +'">');
+
     });
 };
 
@@ -47,4 +64,17 @@ mask.addEventListener("click", function(){
     overlay.classList.add("noshow");
 });
 
-// 抓資料
+
+
+
+
+/*
+document.querySelectorAll(".item").forEach(function(item){
+    item.addEventListener('click', function(){
+        mask.classList.remove("noshow");
+        mask.classList.add("show");
+        overlay.classList.remove("noshow");
+        overlay.classList.add("show");
+    })
+})
+*/
