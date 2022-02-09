c4569235e047770637a15bedaf05ba0f01d61f99
rebeccahsu <rebeccahsuhth@gmail.com>
2022-02-09 15:16:55 +0800

    [index] header scroll

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,45 @@
+var header = document.getElementById("header");
+header.setAttribute("style", "background-color: rgba(0, 0, 0, 0);")
+var bctext = document.querySelector("div.bc-text");
+bctext.setAttribute("style", "opacity: .1;");
+
+// ----- Scroll 事件 -------
+window.addEventListener("scroll", function(){
+    console.log(window.scrollY);
+
+    // ------ Header ------
+    let top_block = document.getElementsByClassName("indextop")[0];
+    // console.log(top_block.offsetHeight);
+    if(window.scrollY >= (top_block.offsetHeight * 0.7)){
+        header.setAttribute("style", "background-color: #26273B);");
+        header.style.transition = "1s";
+    }else{
+        header.setAttribute("style", "background-color: rgba(0, 0, 0, 0);")
+    };
+
+    // ------ Section 1: Brand Concept ------
+    let bc = document.getElementById("bc");
+    let pic1 = document.querySelector("img.bc1");
+    let pic2 = document.querySelector("img.bc2");
+    let pic3 = document.querySelector("img.bc3");
+    let narrvl = document.getElementById("narrvl");
+    
+    // pic1.setAttribute("style", "transform: translateX(-100px);");
+    
+
+    if(window.scrollY >= (bc.offsetTop * 0.5)){
+        console.log("here");
+        // pic1.setAttribute("style", "transform: translateX(100px);")
+        let bctext = document.querySelector("div.bc-text");
+        bctext.setAttribute("style", "opacity: 1;");
+        bctext.style.transition = "3s";
+    };
+});
+
+
+
+// ----- Scroll 事件 結束 -------
+
 //slider
 // var slideIndex = 0;
 // slidesShow();
