4ac802b81268b802a26046eba3a037a4a4fded68
rebeccahsu <rebeccahsuhth@gmail.com>
2022-02-11 17:36:20 +0800

    [lookbook] marqee

--- javascript/all.js
+++ javascript/all.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Hamburger
 
 
@@ -10,9 +12,9 @@ function scroll(element){
         }else{
             element.classList.remove("active");
         };
-    console.log(window.scrollY + window.innerHeight)
-    console.log("高度/3: " + (element.offsetHeight/3))
-    console.log("頂部位置: " + element.offsetTop)   
+    // console.log(window.scrollY + window.innerHeight)
+    // console.log("高度/3: " + (element.offsetHeight/3))
+    // console.log("頂部位置: " + element.offsetTop)   
     }); 
     
 }
@@ -70,3 +72,26 @@ scroll(fd_img);
 // };
 // let emodel = document.querySelector("img.emodel");
 // scroll(emodel);
+
+//-----------[lookbook]-------------------
+//marqee
+var ul_marqee = document.getElementsByClassName("marqeepics")[0];
+console.log(ul_marqee);
+console.log(typeof(ul_marqee));
+console.log(ul_marqee.innerHTML);
+
+var arr_marqee = [ul_marqee.innerHTML];
+console.log(arr_marqee);
+arr_marqee.push(ul_marqee.innerHTML);
+
+
+setInterval(function(){
+    arr_marqee.push(ul_marqee.innerHTML);
+    ul_marqee.insertAdjacentHTML("beforeend", arr_marqee[1]);
+    console.log("10s");
+  }, 10000);
+
+setInterval(function(){
+    arr_marqee.shift();
+    console.log("shift");
+}, 20000);
