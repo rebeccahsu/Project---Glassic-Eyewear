ca83be10994d1fa66ab08c1abd170b4cd63a462f
rebeccahsu <rebeccahsuhth@gmail.com>
2022-02-24 23:17:22 +0800

    update: item img 600x600

--- javascript/items.js
+++ javascript/items.js
@@ -1,27 +1,23 @@
 // -------- Overlay ----------
 
-let item = document.getElementsByClassName("item");
-// console.log(item);
-// var item2 = document.querySelectorAll(".item");
-// console.log(item2);
+let gitem = document.getElementsByClassName("gitem");
 let overlay = document.getElementById("overlay");
-// console.log(overlay);
 let mask = document.getElementById("mask");
-
 let opic = document.getElementById("opic");
-
 let itemname = document.getElementsByClassName("itemname");
 let oname = document.getElementById("oname");
-
 let ocolor = document.getElementById("ocolor");
 let osize = document.getElementById("osize");
 let omaterial = document.getElementById("omaterial");
 
+// let opicimg = document.querySelector("img#opicimg");
+// console.log(opicimg);
+// let opicimg2 = document.querySelector("img#opicimg2");
 
-for(let i = 0; i < item.length; i++){
+for(let i = 0; i < gitem.length; i++){
     // console.log(item[i]);
 
-    item[i].addEventListener("click", function(){
+    gitem[i].addEventListener("click", function(){
         // console.log("clicked");
         opic.innerHTML = "";
 
@@ -43,8 +39,21 @@ for(let i = 0; i < item.length; i++){
         omaterial.innerHTML = oitemmaterial;
 
         let oitempic = document.getElementsByClassName("ipic")[i].src;
-        opic.insertAdjacentHTML("afterbegin", '<img src="'+ oitempic +'">');
+        opic.insertAdjacentHTML("afterbegin", '<li class="owl-item"><img src="'+ oitempic +'"></li>');
 
+        // let oitempic2 = document.getElementsByClassName("ipic2")[i].src;
+        // opic.insertAdjacentHTML("beforeend", '<li class="owl-item"><img src="'+ oitempic2 +'"></li>');
+
+        // let oitempic = document.getElementsByClassName("ipic")[i].src;
+        
+        // console.log(oitempic);
+        // opicimg.setAttribute("src", oitempic);
+
+        // let oitempic2 = document.getElementsByClassName("ipic2")[i].getAttribute("src");
+        
+        // opicimg2.setAttribute("src", oitempic2);
+        // opic.classList.add("owl-carousel");
+        // opic.classList.add("owl-theme");
     });
 };
 
@@ -65,12 +74,28 @@ mask.addEventListener("click", function(){
 });
 
 
-
+// --------- Carousel ------------
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 1
+      }
+    }
+});
 
 
 /*
-document.querySelectorAll(".item").forEach(function(item){
-    item.addEventListener('click', function(){
+document.querySelectorAll(".gitem").forEach(function(gitem){
+    gitem.addEventListener('click', function(){
         mask.classList.remove("noshow");
         mask.classList.add("show");
         overlay.classList.remove("noshow");
