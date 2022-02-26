ff24c14a686befc807fedad4f202138d407fdf5e
rebeccahsu <rebeccahsuhth@gmail.com>
2022-02-26 23:38:11 +0800

    feat: item 放大鏡

--- javascript/items.js
+++ javascript/items.js
@@ -60,3 +60,40 @@ var swiper = new Swiper(".mySwiper", {
   clickable: true,
   },
 });
+
+// Magnifier
+function zoom_on(e){
+  let magnifier = document.querySelector("div#magnifier");
+  let img_zoom = document.querySelector("img.swiper-slide-active");
+  if(magnifier.style.backgroundImage == ""){
+    magnifier.style.backgroundImage = `url("${img_zoom.src}")`;
+  }else{
+    magnifier.style.backgroundImage = `url("${img_zoom.src}")`
+  };
+  magnifier.style.backgroundPosition = `${magnifier.clientWidth / 2 - e.offsetX * 2}px ${magnifier.clientHeight / 2 - e.offsetY * 2}px`;
+  magnifier.classList.add("-on");
+  magnifier.style.left = e.offsetX + 40 + "px";
+  magnifier.style.top = e.offsetY + 60 +"px";
+};
+
+function zoom_off(){
+  document.getElementById("magnifier").classList.remove("-on");
+};
+
+function zoom_event(){
+  $(".swiper-slide").off("mousemove", zoom_on);
+  $(".swiper-slide").off("mouseout", zoom_off);  
+
+  if(window.innerWidth >= 768){
+    $(".swiper-slide").on("mousemove", zoom_on);
+    $(".swiper-slide").on("mouseout", zoom_off);    
+  }
+};
+
+window.addEventListener("load", function(){
+  zoom_event();
+});
+
+window.addEventListener("resize", function(){
+  zoom_event();
+});
