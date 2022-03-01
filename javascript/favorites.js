62d9719d7372be98255ddbe74c08ea68d025e545
rebeccahsu <rebeccahsuhth@gmail.com>
2022-03-01 16:41:58 +0800

    fix: favorites.js bug

--- javascript/favorites.js
+++ javascript/favorites.js
@@ -50,48 +50,40 @@ document.addEventListener("DOMContentLoaded", function () {
     let ocolor = document.getElementById("ocolor");
     let osize = document.getElementById("osize");
     let omaterial = document.getElementById("omaterial");
-  
-    for(let i = 0; i < gitem.length; i++){
-  
-        gitem[i].addEventListener("click", function(){
-          mask.classList.toggle("noshow");
-          overlay.classList.toggle("noshow");
-  
-          let oitemname = itemname[i].innerHTML;
-          oname.innerHTML = oitemname;
-  
-          let oitemcolor = document.getElementsByClassName("icolor")[i].innerHTML;
-          ocolor.innerHTML = oitemcolor;
-  
-          let oitemsize = document.getElementsByClassName("size")[i].innerHTML;
-          osize.innerHTML = oitemsize;
-  
-          let oitemmaterial = document.getElementsByClassName("material")[i].innerHTML;
-          omaterial.innerHTML = oitemmaterial;
-  
-          let swiperel = document.getElementsByClassName("swiper-slide");
-          let oitempic = document.getElementsByClassName("ipic")[i].src;
-          swiperel[0].setAttribute("src", oitempic);
-          let oitempic2 = document.getElementsByClassName("ipic2")[i].src;
-          swiperel[1].setAttribute("src", oitempic2);
-  
-          // like button
-          let l_items = JSON.parse(localStorage.getItem("l_items"));
-          let h_filled = document.querySelector("img.heart_filled");
-  
-          for(let i = 0; i < l_items.length; i++){
-            if (l_items[i].name == oname.innerHTML){
-              h_filled.classList.add("-on");
-              break;
-            }else{
-              h_filled.classList.remove("-on");
-            }
-  
-          };
-  
-        });
-    };
-  
+    
+    document.addEventListener("click", function(e){
+      console.log(e.target);
+      if(e.target.classList.contains("ipic") || e.target.classList.contains("itemname")){
+        mask.classList.toggle("noshow");
+        overlay.classList.toggle("noshow");
+
+        oname.innerHTML = e.target.closest(".gitem").querySelector("h4.itemname").innerHTML;
+        ocolor.innerHTML = e.target.closest(".gitem").querySelector("p.icolor").innerHTML;
+        osize.innerHTML = e.target.closest(".gitem").querySelector("p.size").innerHTML;
+        omaterial.innerHTML = e.target.closest(".gitem").querySelector("p.material").innerHTML;
+
+        let swiperel = document.getElementsByClassName("swiper-slide");
+        let oitempic1 = e.target.closest(".gitem").querySelector("img.ipic").src;
+        swiperel[0].setAttribute("src", oitempic1);
+        let oitempic2 = e.target.closest(".gitem").querySelector("img.ipic2").src;
+        swiperel[1].setAttribute("src", oitempic2);
+
+        // like button
+        let l_items = JSON.parse(localStorage.getItem("l_items"));
+        let h_filled = document.querySelector("img.heart_filled");
+
+        for(let i = 0; i < l_items.length; i++){
+          if (l_items[i].name == oname.innerHTML){
+            h_filled.classList.add("-on");
+            break;
+          }else{
+            h_filled.classList.remove("-on");
+          }
+
+        };
+      };
+    });
+
     let xbutton = document.getElementsByClassName("close")[0];
     // let firstdot = document.querySelector("span.swiper-pagination-bullet");
     xbutton.addEventListener("click", function(){
